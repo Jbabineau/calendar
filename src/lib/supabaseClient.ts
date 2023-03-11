ebe20a928a11603c8197e417d1069e4ee0060ddc
Jamie Babineau <babineau.james@gmail.com>
2023-03-10 22:50:35 -0500

    fix: updating the project clients

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient('https://ulnxfscflgflfuorundn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsbnhmc2NmbGdmbGZ1b3J1bmRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgyNDc3ODMsImV4cCI6MTk5MzgyMzc4M30.LvpJgY6Z9_o7XIssat8xCOGnXy9hfQaZ1etXIwqv5kw')
+export const supabase = createClient('https://hnukrbmgxkeptnkotxah.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhudWtyYm1neGtlcHRua290eGFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg1MDYzNjMsImV4cCI6MTk5NDA4MjM2M30.4tuisq0UYh3Xh9OWiF3Aqxgih9aD94YpU00WvUHMoNE')
